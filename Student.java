448d8348658ae88734a659e9a3c448d132991e31
Milky19 <97377003+Milky19@users.noreply.github.com>
2025-11-30 19:39:54 +0530

    Removed
    Removed welcome messages from the output.

--- Student.java
+++ Student.java
@@ -8,9 +8,6 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
-         System.out.println("WELCOME TO JENKINS");
-         System.out.println("GOOD EVENING");
-        
     }
 }
        
