338135f0615b554fb6adfa04f4d0b9d593d7e8b4
Milky19 <97377003+Milky19@users.noreply.github.com>
2026-07-21 08:13:37 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,6 +9,6 @@ public class Student {
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("Thanks for using My Application");
         System.out.println("Thanks for joing today session Jenkins");
-        
+        System.out.println("Bye BYe Bye Bye----");
     }
 }
